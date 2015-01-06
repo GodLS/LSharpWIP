70483000b09faf32875f65cdc0cbe7fd7e45599a
DZ191 <darkprince97@live.it>
2015-01-06 02:57:54 +0100

    Fix on Drawings
    Fix on Q Drawin

--- PennyJinx/PennyJinx/PennyJinx.cs
+++ PennyJinx/PennyJinx/PennyJinx.cs
@@ -130,7 +130,7 @@ namespace PennyJinx
                 var DrawW = Menu.Item("DrawW").GetValue<Circle>();
                 var DrawE = Menu.Item("DrawE").GetValue<Circle>();
                 var DrawR = Menu.Item("DrawR").GetValue<Circle>();
-                var QRange = IsFishBone() ? Orbwalking.GetRealAutoAttackRange(null) : Orbwalking.GetRealAutoAttackRange(null) + GetFishboneRange();
+                var QRange = IsFishBone() ? GetMinigunRange(null) : GetMinigunRange(null) + GetFishboneRange();;
                 if (DrawQ.Active) { Utility.DrawCircle(Player.Position,QRange,DrawQ.Color);}
                 if (DrawW.Active) { Utility.DrawCircle(Player.Position, _w.Range, DrawW.Color); }
                 if (DrawE.Active) { Utility.DrawCircle(Player.Position, _e.Range, DrawE.Color); }
